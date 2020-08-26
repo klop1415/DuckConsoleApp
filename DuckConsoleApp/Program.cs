e989bd04612b3f45d57370ee1cf4043c7c0e14c7
Qing Wa <2922400@gmail.com>
2020-08-27 00:05:27 +0700

    Добавьте файлы проекта.

---
+++ DuckConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckConsoleApp
+{
+    public interface FlyMethods
+    {
+        void Fly();
+    }
+    public class Duck : FlyMethods
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Flying");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            FlyMethods flyMethods = new Duck();
+            flyMethods.Fly();
+            
+        }
+
+    }
+}
